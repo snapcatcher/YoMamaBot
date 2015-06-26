97fdeb172531a1ff1f2e7be2ac9a2a08e9b36ef9
Dennis Engelbert <dennis@snapcatcher.de>
2015-06-26 18:01:45 +0200

    first working version

---
+++ YoMamaBot.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import json
+import requests
+import sys
+import os
+import random 
+
+def request(url):
+    req = requests.get(url)
+    if req.status_code == 200:
+        return req.json()
+    else:
+        return str(req.status_code)
+
+def sendMessage(chat_id, message):
+    print "sending Message = " + message
+    url = base_url + 'sendMessage?chat_id=' + chat_id + '&text=' + message
+    request(url)
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+lines = open(current_directory + '/jokes.txt').read().splitlines()
+myline = random.choice(lines)
+
+token = "<your bot token here>"
+base_url = "https://api.telegram.org/bot" + token + "/"
+
+chat_id = sys.argv[1]
+message = sys.argv[2]
+
+if message == "/YoMama" or message == "/yomama":
+    sendMessage(chat_id, myline)
+
